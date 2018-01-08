c59235a4d91b80174bf8f3324ee59549a21cfa82
YongshengRen <1145254583@qq.com>
2018-01-08 22:30:33 +0800

    提交然之的登陆测试程序（元素定位/报告/ddt/日志打印）

--- TestItemsCode/Lib/utils.py
+++ TestItemsCode/Lib/utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # author: samren
-import shutil
+import shutil #主要是对文件的相关操作
 import os
+
 
 shutil.copyfile("HTMLTestRunner.py", "c:\HTMLTestRunner.py")
 
-os.system("del c:\\HTMLTestRunner.py")
+os.system("del c:\\HTMLTestRunner.py") #调用windows下的命令行cmd
---
+++ TestItemsCode/RYS_Test_Code/ranzhi_login_testcodes/ranzhi_login_test.py
@@ -0,0 +1,41 @@
+import unittest
+import time
+from selenium import webdriver
+import ddt #可以将将相同流程的代码通过数据驱动ddt进行参数化
+import logging
+import traceback
+
+test_data = [['admin','123456','退出'],
+             ['','','登录失败']]
+@ddt.ddt
+class RanzhiLogin(unittest.TestCase):
+    """
+    设置谷歌浏览器的打开函数
+    """
+    def setUp(self):
+        self.driver = webdriver.Chrome()  #调用selenium中的webdriver打开谷歌浏览器
+        self.driver.implicitly_wait(30)   #打开谷歌浏览器给其30s的自由等待时间，过时五任何操作关闭浏览器
+        self.base_url = "http://localhost/" #将要打开的链接的头部当作为固定的参数
+        logging.info("已经成功打开谷歌浏览器")
+    def tearDown(self):
+        """声明函数推出谷歌浏览器"""
+        self.driver.quit() #调用webdriver中的quit（）函数退出谷歌浏览器
+        logging.info("已将调用的谷歌浏览器关闭")
+    @ddt.unpack
+    @ddt.data(*test_data)
+    def test_ranzhi_login_username(self,admin,password,flags):
+        """对用户名的测试"""
+        logging.info("test_ranzhi_login_username_test start.....")
+        driver = self.driver
+        driver.get(self.base_url+"ranzhi/www/sys/user-login.html") #调用webdriver中的get()函数再浏览器的地址栏输入链接
+        time.sleep(3) #等待3s主要是防止加载的速率慢影响后续的操作
+        driver.find_element_by_id("account").send_keys(admin) #使用webdriver中的find_element_by_id()函数定位元素，并在Input输入框内通过send_keys()函数输入admin
+        driver.find_element_by_id("password").send_keys(password)
+        driver.find_element_by_id("submit").click() #定位到元素点击按钮
+        time.sleep(3) #如果此处不等待有可能下面的断点判读就会失败
+        self.assertIn(flags,driver.page_source)#加入断言的判断
+        logging.info("test datas is:{},{},{}".format(admin,password,flags))
+
+
+if __name__ == '__main__':
+    unittest.main()
